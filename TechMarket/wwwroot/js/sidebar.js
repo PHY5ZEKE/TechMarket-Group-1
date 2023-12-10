dbfe18a7c185ffa0a8b84bd0208414eb47b7c553
RodPoli <142408199+RodPoli@users.noreply.github.com>
2023-12-11 00:20:13 +0800

    Changed Sidenav
    Changed Sidenav style

---
+++ TechMarket/wwwroot/js/sidebar.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+const sidebar = document.querySelector(".sidebar");
+const submenuItems = document.querySelectorAll(".submenu_item");
+
+
+submenuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("show_submenu");
+        submenuItems.forEach((item2, index2) => {
+            if (index !== index2) {
+                item2.classList.remove("show_submenu");
+            }
+        });
+    });
+});
+
+if (window.innerWidth < 768) {
+    sidebar.classList.add("close");
+} else {
+    sidebar.classList.remove("close");
+}
